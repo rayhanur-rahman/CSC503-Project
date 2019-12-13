d80e375ef131722961e3a9c66b5b607d39bd7048
Md Rayhanur Rahman <rayhan1.618@gmail.com>
2019-12-12 20:15:28 -0500

    ...

--- facts-ansible.pl
+++ facts-ansible.pl
@@ -1936,3 +1936,48 @@ statement(0, language(ansible), file(path__home_rr_Workspace_CSC503__Project_rep
 
 statement(0, language(ansible), file(path__home_rr_Workspace_CSC503__Project_repo__openstack_openstack__ansible__role__systemd_mount_tests_test_), funcName(null), funcArgs(0), funcArgNameInContext(null), funcArgValueInContext(null), funcAction(null), varNamePattern(user), varValue(any), attrNamePattern(null), attrValue(null), dictKeyPattern(null), dictValue(null), isTryStatement(no), isExceptBlockSingleLine(null), passInExceptBlock(null), continueInExceptBlock(null), httpWritePerformedInStatement(no), hashFuncAppliedInSource(null), stringContains(null)).
 
+debugList([debug, debug_propagate_exception]).
+
+hardcodedSecretIdentifierList([key, id, cert, root, passno, pass-no, pass_no, auth_token, authetication_token, auth-token, authentication-token, user, uname, username, user-name, user_name, owner-name, owner_name, owner, admin, login, pass, pwd, password, passwd, secret, uuid, crypt, certificate, userid, loginid, token, ssh_key, md5, rsa, ssl_content, ca_content, ssl-content, ca-content, ssh_key_content, ssh-key-content, ssh_key_public, ssh-key-public, ssh_key_private, ssh-key-private, ssh_key_public_content, ssh_key_private_content, ssh-key-private-content,token, secret, secrete, ssh-key-private-content]).
+
+shellFunctionList([popen, call, check_call, check_output, run, system, popen, popen2, popen3,popen4, execl,execle,execlp,execlpe,execv,execve,execvp,execvpe,spawnl,spawnle,spawnlp,spawnlpe,spawnv,spawnve,spawnvp,spawnvpe,startfile, poepn2, popen3, popen4,getoutput, getstatusoutput]).
+
+
+badFilePermission(Line, Lang, Path) :- statement(Line, language(Lang), file(Path), funcName(chmod), funcArgs(2),_, _, funcAction(group-x), _, _, _, _, _, _, _, _, _, _, _, _, _); statement(Line, language(Lang), file(Path), funcName(chmod), funcArgs(2),_, _, funcAction(world-w), _, _, _, _, _, _, _, _, _, _, _, _, _).
+
+debugInDeployment(Line, Lang, Path) :- statement(Line, language(Lang), file(Path), _, _,_, _, _, varNamePattern(X), varValue(yes), _, _, _, _, _, _, _, _, _, _, _), debugList(L), member(X, L);
+statement(Line, language(Lang), file(Path), funcName(run), _,funcArgNameInContext(X), funcArgValueInContext(yes), _, _,_, _, _, _, _, _, _, _, _, _, _, _), debugList(L), member(X, L).
+
+hardcodedSecret(Line, Lang, Path) :- statement(Line, language(Lang), file(Path), _, _, _, _, _, varNamePattern(X), varValue(any), _, _, _, _, _, _, _, _, _, _, _), hardcodedSecretIdentifierList(L), member(X, L); 
+statement(Line, language(Lang), file(Path), _, _, _, _, _, _, _, attrNamePattern(X), attrValue(any), _, _, _, _, _, _, _, _, _), hardcodedSecretIdentifierList(L), member(X, L);
+statement(Line, language(Lang), file(Path), _, _, _, _, _, _, _, _, _, dictKeyPattern(X), dictValue(any), _, _, _, _, _, _, _), hardcodedSecretIdentifierList(L), member(X, L);
+statement(Line, language(Lang), file(Path), _, _, funcArgNameInContext(X), funcArgValueInContext(any), _, _, _, _, _, _, _, _, _, _, _, _, _, _), hardcodedSecretIdentifierList(L), member(X, L).
+
+emptyPassword(Line, Lang, Path) :- statement(Line, language(Lang), file(Path), _, _, _, _, _, varNamePattern(X), varValue(empty), _, _, _, _, _, _, _, _, _, _, _), hardcodedSecretIdentifierList(L), member(X, L); 
+statement(Line, language(Lang), file(Path), _, _, _, _, _, _, _, attrNamePattern(X), attrValue(empty), _, _, _, _, _, _, _, _, _), hardcodedSecretIdentifierList(L), member(X, L);
+statement(Line, language(Lang), file(Path), _, _, _, _, _, _, _, _, _, dictKeyPattern(X), dictValue(empty), _, _, _, _, _, _, _), hardcodedSecretIdentifierList(L), member(X, L).
+
+
+execUsed(Line, Lang, Path) :-  statement(Line, language(Lang), file(Path), funcName(exec), _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _).
+
+hardcodedBinding(Line, Lang, Path) :- statement(Line, language(Lang), file(Path), funcName(bind), _, _, funcArgValueInContext(ip), _, _, _, _, _, _, _, _, _, _, _, _, _, _); 
+statement(Line, language(Lang), file(Path), _, _, _, _, _, varNamePattern(any), varValue(ip), _, _, _, _, _, _, _, _, _, _, _).
+
+
+
+hardcodedTmpDirectory(Line, Lang, Path) :- statement(Line, language(Lang), file(Path), _, _, _, _, _, _, _, _, _, _, _, _, _, _, _ , _, _, stringContains(tmp)).
+
+ignoreExceptBlock(Line, Lang, Path) :- statement(Line, language(Lang), file(Path), _, _, _, _, _, _, _, _, _, _, _, isTryStatement(yes), isExceptBlockSingleLine(yes), passInExceptBlock(yes),_, _, _, _) ; statement(Line, language(Lang), file(Path), _, _, _, _, _, _, _, _, _, _, _, isTryStatement(yes), isExceptBlockSingleLine(yes), _, continueInExceptBlock(yes), _, _, _).
+
+noIntegrityCheck(Line, Lang, Path) :- statement(Line, language(Lang), file(Path), _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, httpWritePerformedInStatement(yes), hashFuncAppliedInSource(no) , _).
+
+noCertificateValidation(Line, Lang, Path) :-  statement(Line, language(Lang), file(Path), funcName(get), _, funcArgNameInContext(verify), funcArgValueInContext(no), _, _, _, _, _, _, _, _, _, _, _, _, _, _).
+
+useOfHttpWithoutTLS(Line, Lang, Path) :-  statement(Line, language(Lang), file(Path), _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, stringContains(http)).
+
+sqlInjection(Line, Lang, Path) :- statement(Line, language(Lang), file(Path), _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, stringContains(parameterizedSql)).
+
+shellInjection(Line, Lang, Path) :- statement(Line, language(Lang), file(Path), _, _, _, _, _, _, _, attrNamePattern(argv), attrValue(any), _, _, _, _, _, _, _, _, _);
+statement(Line, language(Lang), file(Path), funcName(argumentParser), _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _);
+statement(Line, language(Lang), file(Path), _, _, funcArgNameInContext(shell), _, _, _, _, _, _, _, _, _, _, _, _, _, _, _);
+statement(Line, language(Lang), file(Path), funcName(X), _, funcArgNameInContext(shell), _, _, _, _, _, _, _, _, _, _, _, _, _, _, _), shellFunctionList(L), member(X,L). 
